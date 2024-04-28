5be16371a367c6cb4d2d82d4f817d7b4eeb278d1
Alondra Alfaro Bonilla <107895705+aanahid@users.noreply.github.com>
2024-04-28 17:37:41 -0400

    More icons

--- frontend/src/Components/ListForm.js
+++ frontend/src/Components/ListForm.js
@@ -2,6 +2,8 @@ import React, {useState} from "react"
 import axios from 'axios';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome" 
 import { faSquarePlus } from "@fortawesome/free-solid-svg-icons"
+import { faSquareMinus } from "@fortawesome/free-solid-svg-icons"
+import { faCircleCheck } from "@fortawesome/free-regular-svg-icons"
 
 export const ListForm = ({ fetchItems }) => {
     const [title, setTitle] = useState("");
@@ -30,19 +32,28 @@ export const ListForm = ({ fetchItems }) => {
 
     return (
         <div>
-            <button onClick={handleClick}>
-                <FontAwesomeIcon icon={ faSquarePlus }/> New List
-            </button>
+            {!showInput && (
+                <button onClick={handleClick}>
+                    <FontAwesomeIcon icon={ faSquarePlus }/> New List
+                </button>
+            )}
             {showInput && (
-                <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    value={title}
-                    onChange={handleChange}
-                    placeholder="Enter something..."
-                />
-                <button type="submit">Submit</button>
-                </form>
+                <div>
+                    <button onClick={handleClick}>
+                        <FontAwesomeIcon icon={ faSquareMinus }/> New List
+                    </button>
+                    <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        value={title}
+                        onChange={handleChange}
+                        placeholder="Enter something..."
+                    />
+                    <button type="submit">
+                        <FontAwesomeIcon icon={faCircleCheck} />
+                    </button>
+                    </form>
+                </div>
             )}
         </div>
     );
--- frontend/src/Components/TaskForm.js
+++ frontend/src/Components/TaskForm.js
@@ -2,6 +2,8 @@ import React, {useState} from "react"
 import axios from 'axios';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome" 
 import { faCirclePlus } from "@fortawesome/free-solid-svg-icons"
+import { faCircleMinus } from "@fortawesome/free-solid-svg-icons"
+import { faCircleCheck } from "@fortawesome/free-regular-svg-icons"
 
 export const TaskForm = ({ fetchItems, id }) => { 
     const [title, setTitle] = useState("");
@@ -30,19 +32,28 @@ export const TaskForm = ({ fetchItems, id }) => {
 
     return (
         <div>
-            <button onClick={handleClick}>
-                <FontAwesomeIcon icon={ faCirclePlus }/> New Task
-            </button>
+            {!showInput && (
+                <button onClick={handleClick}>
+                    <FontAwesomeIcon icon={ faCirclePlus }/> New Task
+                </button>
+            )}
             {showInput && (
-                <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    value={title}
-                    onChange={handleChange}
-                    placeholder="Enter something..."
-                />
-                <button type="submit">Submit</button>
-                </form>
+                <div>
+                    <button onClick={handleClick}>
+                        <FontAwesomeIcon icon={ faCircleMinus }/> New Task
+                    </button>
+                    <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        value={title}
+                        onChange={handleChange}
+                        placeholder="Enter something..."
+                    />
+                    <button type="submit">
+                        <FontAwesomeIcon icon={faCircleCheck} />
+                    </button>
+                    </form>
+                </div>
             )}
         </div>
     );
